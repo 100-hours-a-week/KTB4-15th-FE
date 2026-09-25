d1fcee9f6578c7a3c656423107ac70dec4c244eb
vinny-ahn <vinny.ahn@gmail.com>
2026-09-25 20:43:40 +0900

    feat: 가상 피팅 결과 출력 페이지 구현

--- src/app/(service)/(main)/fitting/jobs/[fittingJobId]/page.tsx
+++ src/app/(service)/(main)/fitting/jobs/[fittingJobId]/page.tsx
@@ -1,6 +1,4 @@
 import { FittingJobScreen } from "@/features/fitting";
-import { Header, HeaderIconLink, HeaderTitle } from "@/shared/ui/header";
-import { BackIcon } from "@/shared/ui/icon";
 
 export default async function FittingJobPage({
   params,
@@ -8,17 +6,5 @@ export default async function FittingJobPage({
   const { fittingJobId } = await params;
   const parsedFittingJobId = Number(fittingJobId);
 
-  return (
-    <>
-      <Header
-        center={<HeaderTitle>가상 피팅</HeaderTitle>}
-        left={
-          <HeaderIconLink aria-label="피팅으로 돌아가기" href="/fitting">
-            <BackIcon />
-          </HeaderIconLink>
-        }
-      />
-      <FittingJobScreen fittingJobId={parsedFittingJobId} />
-    </>
-  );
+  return <FittingJobScreen fittingJobId={parsedFittingJobId} />;
 }
